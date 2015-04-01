d95e21c33504b9436ea02ca6e548582f861ab2eb
Chris Johnson <csj790@yahoo.com>
2015-04-01 04:25:47 +0000

    removed unnecessary calls to str()

--- launchDefensics.py
+++ launchDefensics.py
@@ -15,27 +15,26 @@ args = parser.parse_args()
 #print args
 
 reservation, instances = ([] for l in range (2))
+
+conn = awsModules.connect(args)
 awsDir = awsModules.awsDir()
 
 if not os.path.isdir(awsDir):
   os.makedirs(awsDir)
 
-#Set up connection to AWS
-conn = awsModules.connect(args)
-
 #Create key pair
-if conn.get_key_pair(str(args.name)):
-  if not os.path.exists(awsDir + str(args.name) + '.pem'):
+if conn.get_key_pair(args.name):
+  if not os.path.exists(awsDir + args.name + '.pem'):
     sys.exit("This name is already taken. Please launch again with new name")
 else:
-    key = conn.create_key_pair(str(args.name))
+    key = conn.create_key_pair(args.name)
     key.save(awsDir)
 
 #Create reservation
 reservation = conn.run_instances('ami-ff21c0bb',max_count=args.num, key_name=args.name, security_groups=['sg_training'], instance_type=args.instance, dry_run=args.dry_run)
 instances = reservation.instances
 print "Your reservation id for this defensics request is: " + str(reservation.id)
-print "Please note it may take up to 20 minutes for the images to launch and be fully available"
+print "Please note it may take up to 30 minutes for the images to launch and be fully available"
 print "If you need information on your reservation, please check the local log .aws.log"
 
 #Log instance information
@@ -47,17 +46,17 @@ if not awsDB.fetchone():
   awsDB.execute("create table instances(instance_id, reservation_id, name, public_ip, password, state);")
 
 for index, i in enumerate(instances):
-  commonName = str(args.name) + ':' + str(index) + '_' + str(reservation.id)
-  awsDB.execute("insert into instances values (?,?,?,?,null,?)", (str(i.id), str(reservation.id), commonName, str(i.ip_address), str(i._state)))
+  commonName = args.name + ':' + str(index) + '_' + reservation.id
+  awsDB.execute("insert into instances values (?,?,?,?,null,?)", (i.id, reservation.id, commonName, i.ip_address, str(i._state)))
   i.add_tag('Name',value=commonName)
   i.add_tag('Admin',value=args.admin)
   i.add_tag('Status',value='training')
   with open('.aws.log', 'a') as log:
     log.write(str(datetime.datetime.now()) + '\n')
-    log.write('reservation id: ' + str(reservation.id) + '\n')
-    log.write('base name: ' + str(args.name) + '\n')
+    log.write('reservation id: ' + reservation.id + '\n')
+    log.write('base name: ' + args.name + '\n')
     log.write('instances: ' + str(instances) +'\n')
-    log.write('Please note it may take up to 20 minutes for the images to launch and be fully available')
+    log.write('Please note it may take up to 30 minutes for the images to launch and be fully available')
     log.write('\n\n')
   log.closed
 
