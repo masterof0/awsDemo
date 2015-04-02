d26fc83900773cda66927970d9ce30b3caac1b5b
Chris Johnson <csj790@yahoo.com>
2015-04-02 21:40:32 +0000

    Create error handler

--- codeAWS.py
+++ codeAWS.py
@@ -28,7 +28,11 @@ def hello_world():
 def show_instances():
   cur = g.db.execute('select * from instances;')
   instances = [dict(hostname=row[2], instance_id=row[0], reservation_id=row[1], public_ip=row[3], password=row[4], state=row[5]) for row in cur.fetchall()]
-  return render_template('show_entries.html', entries=instances)
+  return render_template('show_entries.html', entries=instances, pageTitle="AWS Instances")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'This page does not exist', 404
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
